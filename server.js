84d65f7745e99d6483048a539fd38446e4a27f0a
LeePoongHo <dlvndghs@naver.com>
2020-09-16 15:56:04 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '이풍호',
-            'birthday' : '930402',
-            'gender' : '남자',
-            'job' : '치킨집사장'
-        },
-        {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '조은별',
-            'birthday' : '941109',
-            'gender' : '여자',
-            'job' : 'CEO'
-        },
-        {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '김대연',
-            'birthday' : '940826',
-            'gender' : '남자',
-            'job' : '제빵사'
+    connection.query(
+        'SELECT * FROM CUSTOMER',   
+        (err, rows, fields) => { 
+            res.send(rows); 
         }
-    ]);
+    );
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
